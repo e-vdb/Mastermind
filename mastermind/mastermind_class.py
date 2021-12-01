4cf6167100d95d9ea71d1462843c7d5b919ea221
emelinevanderbeken <emelinevanderbeken@gmail.com>
2021-12-01 14:49:14 +0100

    Added method to mastermind class

---
+++ mastermind/mastermind_class.py
@@ -0,0 +1,39 @@
+"""Define class Mastermind."""
+
+import numpy as np
+
+
+colors=["red", "yellow", "blue", "white", "orange", "green", "pink", "purple"]
+
+class Mastermind():
+    """
+    A class to represent Mastermind game.
+
+    """
+    def __init__(self):
+        pass
+
+    def setup_combination(self):
+        """
+        Returns the Mastermind code.
+
+        Returns a 1-D array of string containing the names of four colors
+        randomly chosen from the "colors" list.
+
+        """
+        return np.random.choice(colors, size=4)
+
+    def occurenceColors(self, comb):
+        """
+        Returns a dictionary from a list of string
+        counting the occurence of colors in the list
+        (key=color and value = occurence)
+
+        """
+        dicOccurence = {}
+        for color in comb:
+            if color in dicOccurence:
+                dicOccurence[color] += 1
+            else:
+                dicOccurence[color] = 1
+        return dicOccurence
