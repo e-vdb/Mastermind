074bd3e617d3f7bac17d95a3544f4efed04ce591
emelinevanderbeken <emelinevanderbeken@gmail.com>
2021-12-01 12:36:21 +0100

    Added bar with statistics

--- mastermind/GUI.py
+++ mastermind/GUI.py
@@ -3,6 +3,7 @@
 import tkinter as tk
 from game_class import Game
 from help_functions import printRules, about
+from stat_functions import Stat
 
 colors=["red", "yellow", "blue", "white", "orange", "green", "pink", "purple"]
 
@@ -28,7 +29,7 @@ lab_Message.pack(side=tk.TOP)
 
 
 game = Game(can, lab_Message)
-
+stat = Stat()
 ############################################################################
 # Menus
 ############################################################################
@@ -36,6 +37,8 @@ top = tk.Menu(window)
 window.config(menu=top)
 game_menu = tk.Menu(top, tearoff=False)
 top.add_cascade(label='Game', menu=game_menu)
+stat_menu = tk.Menu(top, tearoff=False)
+top.add_command(label='Statistics', command=stat.show_stat)
 help_menu = tk.Menu(top, tearoff=False)
 top.add_cascade(label='Help', menu=help_menu)
 game_menu.add_command(label='Exit', command=window.destroy)
